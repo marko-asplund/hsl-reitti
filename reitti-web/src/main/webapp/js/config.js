0a501e25fdd2db38203f3f784ce75af8a91a6721
marko asplund <marko.asplund@gmail.com>
2012-02-07 22:13:31 +0200

    add config.js

---
+++ reitti-web/src/main/webapp/js/config.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+
+function Config() {
+//	this.jpUrl = "http://api.reittiopas.fi/hsl/prod/";
+	this.jpUrl = "/hsl_reitti-web-0.0.1-SNAPSHOT/p/jp/hsl/prod/";
+	this.jpUser = "foo";
+	this.jpPass = "bar";
+	
+	return true;
+}
